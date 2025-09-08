8330655dfe0612a7b71dd1f49c9882a7557a892f
Juanma <jmmoreno19@gmail.com>
2025-09-09 00:32:33 +0100

    arranque 2

--- postcss.config.js
+++ postcss.config.js
@@ -4,19 +4,5 @@ export default {
     autoprefixer: {},
   },
 }
-```
 
-### **Paso Final: Subir la Configuración**
 
-1.  Asegúrate de que estos tres nuevos archivos (`vite.config.js`, `tailwind.config.js`, `postcss.config.js`) están guardados en la carpeta principal de tu proyecto.
-2.  Abre tu terminal y sube todo a GitHub con los tres comandos de siempre:
-
-    ```bash
-    git add .
-    ```
-    ```bash
-    git commit -m "Añadir archivos de configuración de Vite y Tailwind"
-    ```
-    ```bash
-    git push
-    
